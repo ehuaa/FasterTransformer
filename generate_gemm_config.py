5397967780cd7943d6234bab0694b206c7481636
ehuaa <ehuamail@163.com>
2023-11-26 16:05:28 +0000

    upload generate_gemm_config

---
+++ generate_gemm_config.py
@@ -0,0 +1,28 @@
+import subprocess 
+
+
+def get_bert_gemm():
+    import os.path as osp
+
+    cur_path = osp.abspath('.')
+    bin_path = osp.join(cur_path, 'build', 'bin', 'bert_gemm')
+    assert osp.exists(bin_path), f'{bin_path} not exists'
+    return bin_path
+
+
+def main(head_num: int = 16,
+         size_per_head: int = 64,
+         max_seq_len: int = 512,
+         tensor_para_size: int = 1,
+         max_batch_size: int = 256):
+    for bsz in (64, 128, 256):
+        for seq_len in range(100, max_seq_len + 1):
+            subprocess.call(
+                f'{get_bert_gemm()} {bsz} {seq_len} {head_num} {size_per_head} 1 0 1 1',
+                shell=True)
+
+
+if __name__ == '__main__':
+    import fire
+
+    fire.Fire(main)
